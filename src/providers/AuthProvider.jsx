e10336dffe85882789a4305e7ca783f04e13e58b
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2023-12-21 22:15:48 +0600

    createUser and SignIn user implemented

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,16 +1,25 @@
 import React, { createContext } from 'react';
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-    const user = null;
+    const user = {};
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
 
     const authInfo = {
-        auth,
         user,
+        createUser,
+        signIn
     };
 
     return (
